4389b7f5f9e4e5e188c5bbb36eee2c24b14edb6e
Artem E. Knyazev <artem.e.knyazev@yandex.ru>
2019-07-03 19:39:46 +0300

    Routing&housekeeping

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,49 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Helmet from 'react-helmet';
 
-const App = () => <h1>Hello, World!</h1>;
+const MainPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Main page</title>
+    </Helmet>
+    <h1>Main page</h1>
+  </React.Fragment>
+);
+
+const ContactsPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Contacts</title>
+    </Helmet>
+    <h1>Contacts</h1>
+  </React.Fragment>
+);
+
+const Nav = () => (
+  <nav>
+    <Link to='/'>Main page</Link>
+    <Link to='/contacts'>Contacts</Link>
+  </nav>
+);
+
+const Header = () => (
+  <header>
+    <Nav />
+  </header>
+);
+
+const App = () => (
+  <Router>
+    <Helmet
+      defaultTitle='S3rv3r1355'
+      titleTemplate='S3rv3r1355 — %s'
+    />
+    <Header />
+    <Route path='/' exact component={MainPage} />
+    <Route path='/contacts' exact component={ContactsPage} />
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
