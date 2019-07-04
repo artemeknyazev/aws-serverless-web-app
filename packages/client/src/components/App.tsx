ee77857d07fb7430693b482237e421e8de1e1273
Artem E. Knyazev <artem.e.knyazev@yandex.ru>
2019-07-04 16:47:40 +0300

    [skip travis] Client refactoring&housekeeping

---
+++ packages/client/src/components/App.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom';
+import Helmet from 'react-helmet';
+
+const MainPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Main page</title>
+    </Helmet>
+    <h1>Main page</h1>
+  </React.Fragment>
+);
+
+const ContactsPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Contacts</title>
+    </Helmet>
+    <h1>Contacts</h1>
+  </React.Fragment>
+);
+
+// 404
+const NotFoundPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Not found</title>
+    </Helmet>
+    <h1>Not found</h1>
+  </React.Fragment>
+);
+
+// 5**
+const InternalErrorPage = () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Internal error</title>
+    </Helmet>
+    <h1>Internal error</h1>
+  </React.Fragment>
+);
+
+const Header = () => (
+  <header>
+    <nav>
+      <Link to='/'>Main page</Link>
+      <Link to='/contacts'>Contacts</Link>
+      <Link to='/aaa'>aaa</Link>
+    </nav>
+  </header>
+);
+
+const App = () => (
+  <Router>
+    <Helmet defaultTitle='S3rv3r1355' titleTemplate='S3rv3r1355 &mdash; %s' />
+    <Header />
+    <Switch>
+      <Route path='/' exact component={MainPage} />
+      <Route path='/contacts' exact component={ContactsPage} />
+      <Route path='/not-found' exact component={NotFoundPage} />
+      <Route path='/error' exact component={InternalErrorPage} />
+      <Redirect to='/not-found' />
+    </Switch>
+  </Router>
+);
+
+export default App;
